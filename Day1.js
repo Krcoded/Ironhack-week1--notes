dee6ea07d60068d9867b33a088a32330fb673267
Kevin Rodriguez <87106659+Krcoded@users.noreply.github.com>
2021-08-09 14:20:43 -0400

    update

--- Day1.js
+++ Day1.js
@@ -1,5 +1,7 @@
 //cmd + J 
 
 // git add . # you add files to your git repo 
-// git commit -m "message you want to write"
 // git push origin main # your local repo (in computer) into the cloud/github repo 
+// git commit -m "message you want to write"
+
+
