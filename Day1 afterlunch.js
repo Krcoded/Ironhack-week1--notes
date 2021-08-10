71315b963a3aa055531a6828ff1fd5c549b09151
Kevin Rodriguez <87106659+Krcoded@users.noreply.github.com>
2021-08-10 09:47:10 -0400

    lab practice

--- Day1 afterlunch.js
+++ Day1 afterlunch.js
@@ -1,1 +1,21 @@
-dfkvdmsds
+//Bonus:
+//Check if a word of phraise is a palindrome 
+
+const PhrasePalindrome = 'racecar'; 
+  let isPhrasePalindrome = 'Amor a Roma';
+  isPhraisePalindrome = isPhrasePalindrome.toLowerCase();
+  //'amor a roma'
+isPhrasePalindrome = isPhrasePalinrome.trim()
+// 'amoraroma';
+isPhrasePalindrome = isPhrasePalindrome.raplace(/ /g,'');
+console.log(isPhrasePalindrome);
+isPhrasePalindrom.length-1;
+
+for (let i = 0; i < j; i++, j-- ) {
+  if (isPhrasePalindrome[i] !== isPhrasePalindrome[j]) {
+   console.log("Not a palindrome")
+   break; 
+  }
+  console.log(`character i is: ${isPhrasePalindrome[i]}, character j is ${isPhrasePalindrome[j]}`); 
+}
+
