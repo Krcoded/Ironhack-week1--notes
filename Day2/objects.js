dc81f16b78f61f22062b96f453a5c4c352d7e4cd
Kevin Rodriguez <87106659+Krcoded@users.noreply.github.com>
2021-08-10 15:16:28 -0400

    ojects lesson

--- Day2/objects.js
+++ Day2/objects.js
@@ -1,6 +1,35 @@
 // OBJECTIVE: simple objects, go into food and students object, iterate through objects 
 
 
-const link = {
+let link = {
+    items: ['sword', 'bow', 'boomerang'],
+    hearts: 9,
+    stamina: 9,
+    companion: "Fairy", 
+    crush: "Zelda"
+    
+}
 
+console.log(link.items[1]); 
+
+let inst = {
+    name: "Arturo",
+    food: "Escamoles"
+}
+
+let st2 = {
+    name: "Andrew",
+    food: "Sushi"
+}
+
+let st3 = {
+    name: "Frank",
+    food: "Encebollado" 
+}
+
+
+let students = [st2, st3, inst, ]; 
+
+for(let i = 0; i < students.length; i++) {
+    console.log(`The sutdent ${students[i].name} like to eat ${students[i].food}`);
 }
